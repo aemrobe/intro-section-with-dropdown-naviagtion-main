d285c8d7ca994b429ac51e23815252205a4472af
aemrobe <aemrobekalu083@gmail.com>
2024-01-13 21:37:30 +0300

    completing the mobile and desktop view

--- script.js
+++ script.js
@@ -1,45 +1,92 @@
 const closeBtn = document.querySelector(".close-btn");
 const menuBar = document.querySelector(".header__mobile-menu-img");
 const navbar = document.querySelector(".navbar");
-const collapseItems = document.querySelector(".navbar__collapsed-items");
-const iconCollapse = document.querySelector(".icon-collapse");
+const mainHeroImg = document.querySelector(".main__hero-img");
 
-iconCollapse.addEventListener("click", function () {
-  collapseItems.classList.remove("hide-the-item");
-  collapseItems.classList.add("showtheItem");
-  iconCollapse.src = `./images/icon-arrow-up.svg`;
-  iconCollapse.alt = "icon which shows the lists are expanded";
+//function which change the collapse icons when navitem is hovered
+const changingTheCollapseIcons = function (e, src) {
+  const navItem = e.target.closest(".navbar__list-item");
 
-  //changing the icon collapse to the icon expanded
-  iconCollapse.setAttribute("class", "icon-expanded");
+  const iconCollapse = navItem.querySelector(".icon-collapse");
 
-  const iconExpanded = document.querySelector(".icon-expanded");
+  //if the navItem doesn't contain the collapse icon return from the function
+  if (!iconCollapse) return;
 
-  iconExpanded.addEventListener("click", function () {
-    console.log("icon expanded");
-    collapseItems.classList.add("hide-the-item");
+  const iconCollapseSrc = iconCollapse.src;
 
-    iconCollapse.src = `./images/icon-arrow-down.svg`;
-    iconCollapse.alt = "icon which shows the lists are collapsed";
+  if (iconCollapseSrc.includes("arrow-down")) {
+    iconCollapse.src = `${src}`;
+  }
+};
 
-    //changing the icon collapse to the icon expanded
-    iconExpanded.setAttribute("class", "icon-collapse");
-
-    collapseItems.addEventListener(
-      "animationend",
-      function () {
-        collapseItems.classList.remove("showtheItem");
-      },
-      {
-        once: true,
-      }
+["mouseover", "mouseout"].forEach((ev) => {
+  navbar.addEventListener(ev, function (e) {
+    changingTheCollapseIcons.call(
+      null,
+      e,
+      ev === "mouseover"
+        ? `./images/icon-arrow-down-black.svg`
+        : `./images/icon-arrow-down.svg`
     );
   });
 });
 
+//function
+//a function which hides and expand the navbar
 const hidingAndDisplayTheNavbar = function () {
   navbar.classList.toggle("move-to-right");
 };
-closeBtn.addEventListener("click", hidingAndDisplayTheNavbar);
 
-menuBar.addEventListener("click", hidingAndDisplayTheNavbar);
+[closeBtn, menuBar].forEach((el) => {
+  el.addEventListener("click", hidingAndDisplayTheNavbar);
+});
+
+//handling the dropdown in the mobile view
+navbar.addEventListener("click", function (e) {
+  const collapseIcon = e.target.closest(".icon-collapse");
+
+  if (!collapseIcon) return;
+
+  const iconSrc = collapseIcon.src;
+  const paragraphBeforeTheIcon = collapseIcon.previousElementSibling;
+
+  const navCollapseItems = collapseIcon.nextElementSibling;
+
+  if (iconSrc.includes("icon-arrow-down")) {
+    collapseIcon.src = `./images/icon-arrow-up-black.svg`;
+    collapseIcon.alt = "icon which shows the drop down menu below is expanded";
+
+    paragraphBeforeTheIcon.style.color = `var(--almost-black)`;
+    navCollapseItems.classList.add("expand-drop-down");
+    navCollapseItems.classList.remove("not-open");
+  } else {
+    collapseIcon.src = `./images/icon-arrow-down.svg`;
+    collapseIcon.alt =
+      "icon which shows the drop down menu below is collpassed";
+    paragraphBeforeTheIcon.style.color = `var(--medium-gray)`;
+    navCollapseItems.classList.remove("expand-drop-down");
+    navCollapseItems.classList.add("collapse");
+
+    setTimeout(function () {
+      navCollapseItems.classList.remove("collapse");
+      navCollapseItems.classList.add("not-open");
+    }, 500);
+  }
+});
+
+const addEvent = function (eventName, callback) {
+  window.addEventListener(eventName, callback);
+};
+
+const changeHeroImgSrc = function () {
+  const windowSize = window.innerWidth;
+
+  if (windowSize >= 1200) {
+    mainHeroImg.src = `./images/image-hero-desktop.png`;
+  } else if (windowSize < 1200) {
+    mainHeroImg.src = `./images/image-hero-mobile.png`;
+  }
+};
+
+addEvent("load", changeHeroImgSrc);
+addEvent("resize", changeHeroImgSrc);
